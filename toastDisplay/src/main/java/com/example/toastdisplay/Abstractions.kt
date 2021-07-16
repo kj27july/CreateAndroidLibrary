365f312f4f69561bfef80d2e1c8e85493af2920d
kj <79355765+kj27july@users.noreply.github.com>
2021-07-16 19:10:56 +0530

    Logcat commented-2

---
+++ toastDisplay/src/main/java/com/example/toastdisplay/Abstractions.kt
@@ -0,0 +1,20 @@
+package com.example.toastdisplay
+
+import android.util.Log
+
+abstract class Abstractions {
+//    private var a: Int? = null
+//    private var b: Int? = null
+//
+////    abstract fun dateOfBirth(date: String)
+//    fun setValues(x: Int, y: Int) {
+//        a = x
+//        b = y
+//    }
+//
+//    fun displayValue() {
+//        Log.d("Values-a:", a.toString())
+//        Log.d("Values-a:", b.toString())
+//
+//    }
+}
