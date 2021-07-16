cf0243cc7ca3f043b61f9ae55b0c7f22827b8351
kj <79355765+kj27july@users.noreply.github.com>
2021-07-16 19:04:30 +0530

    Logcat commented

--- toastDisplay/src/main/java/com/example/toastdisplay/Toaster.kt
+++ toastDisplay/src/main/java/com/example/toastdisplay/Toaster.kt
@@ -7,25 +7,25 @@ import android.widget.Toast
 object Toaster : Abstractions() {
     fun simpleToast(context: Context, msg: String) {
         Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
-        Log.d("displayToast", "simpleToast:1")
+//        Log.d("displayToast", "simpleToast:1")
     }
 
     internal fun sum(a: Int, b: Int): Int {
         return a + b
-        Log.d("displayToast", (a + b).toString())
+//        Log.d("displayToast", (a + b).toString())
     }
 
     fun result(): Int {
         val res:Int=sum(10, 20)
         return  res
-        Log.d("displayToast", res.toString())
+//        Log.d("displayToast", res.toString())
     }
 
 
     object Toaster2 {
         fun simpleToast(context: Context) {
             Toast.makeText(context, "Inside Toaster", Toast.LENGTH_SHORT).show()
-            Log.d("displayToasts", "simpleToast: 2")
+//            Log.d("displayToasts", "simpleToast: 2")
         }
     }
 }
